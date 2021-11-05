1edde7a46aba790c631567bfca2b2d537557652f
Tahi_ri96@itstep.edu.az <Tahi_ri96@itstep.edu.az>
2021-11-05 13:22:38 +0400

    .

--- SimpleTaskManager/ViewModel/MainWindowViewModel.cs
+++ SimpleTaskManager/ViewModel/MainWindowViewModel.cs
@@ -5,12 +5,16 @@ using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
 
 namespace SimpleTaskManager.ViewModel
 {
-    public class MainWindowViewModel:BaseViewModel
+    public class MainWindowViewModel : BaseViewModel
     {
+        public MainWindow MainWindow { get; set; }
         public ObservableCollection<Process> Processes { get; set; }
         private Process selectedProcess;
 
@@ -26,27 +30,65 @@ namespace SimpleTaskManager.ViewModel
 
         public MainWindowViewModel(MainWindow mainWindow)
         {
-            Processes = new ObservableCollection<Process>(Process.GetProcesses());
+            DispatcherTimer dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Tick += DispatcherTimer_Tick;
+            dispatcherTimer.Interval = TimeSpan.FromSeconds(3);
+            dispatcherTimer.Start();
 
-            AddCommand = new RelayCommand((sender => 
+
+            MainWindow = mainWindow;
+            AddCommand = new RelayCommand((sender =>
             {
-
+                mainWindow.wishListView.Items.Add(mainWindow.wishlistTxtbx.Text);
             }));
 
             CreateCommand = new RelayCommand((sender =>
             {
-                Process.Start(mainWindow.processNameTxtbx.Text);
+                if (mainWindow.wishListView.Items == null)
+                {
+                    Process.Start(mainWindow.processNameTxtbx.Text);
+                }
+                else
+                {
+                    try
+                    {
+                        Process.Start(mainWindow.processNameTxtbx.Text);
+                        Thread.Sleep(4000);
+                        foreach (var item in mainWindow.wishListView.Items)
+                        {
+                            var process = Processes.FirstOrDefault(p => p.ProcessName == item.ToString());
+                            process.Kill();
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
             }));
 
             EndCommand = new RelayCommand((sender =>
             {
-                if (SelectedProcess != null)
+                try
                 {
-                    SelectedProcess.Kill();
+                    if (SelectedProcess != null)
+                    {
+                        SelectedProcess.Kill();
+                    }
                 }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
             }));
-            
+
         }
 
+        private void DispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            Processes = new ObservableCollection<Process>(Process.GetProcesses());
+            MainWindow.propcessesListView.ItemsSource = Processes;
+        }
     }
 }
